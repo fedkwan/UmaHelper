1fd8bc38789e77a8eed9f2dfe9a9f781802514e8
FedKwan <fedkwan@qq.com>
2024-05-29 17:22:49 +0800

    加入一个判断下一页的功能

---
+++ setting/page_dict.py
@@ -0,0 +1,24 @@
+page_dict = {
+
+    "app_main":{
+        "point" : ["", "", "", ""],
+        "sub_image" : "",
+        "times": 1,
+        "expect_page_list": []
+    },
+
+    "app_main":{
+        "point" : ["", "", "", ""],
+        "sub_image" : "",
+        "times": 1,
+        "expect_page_list": []
+    },
+
+    "main": {
+        "point" : ["", "", "", ""],
+        "sub_image" : "",
+        "times": 1,
+        "expect_page_list": []
+    }
+
+}
