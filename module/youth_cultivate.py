a83f44d4528209fd618a16086c172699da7c1b0e
fedkwan <fedkwan@qq.com>
2024-07-06 11:55:54 +0800

    缺了个参数

--- module/youth_cultivate.py
+++ module/youth_cultivate.py
@@ -130,7 +130,7 @@ def page_action(page):
         return
 
     if page == "event":
-        event(d, screen, setting_dic)
+        event(d, screen, p_ocr, setting_dic)
         time.sleep(DEFAULT_SLEEP_TIME)
         return
 
